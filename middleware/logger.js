9f4ad0ffcc174dc95b69a5a80de07e1da3654beb
Tamjid-Karim-1 <riyadhrrkk10@gmail.com>
2025-11-24 18:05:54 +0000

    logger file

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,1 +1,7 @@
-// logger middleware
+const logger = (req, res, next) => {
+    console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url}`);
+    
+    next(); 
+};
+
+module.exports = logger;
